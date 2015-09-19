4956f2605fe715a25ec13e4cbf954de83c4b6617
Vitalij Karpusa <vitalij@karpusa.lv>
2015-09-19 21:41:46 +0300

    Template add ad and get category list

--- app/controllers/add.js
+++ app/controllers/add.js
@@ -5,8 +5,17 @@ angular
     .controller('AddCtrl', AddCtrl);
 
 function AddCtrl($rootScope, $scope, $http, config) {
+    var ad = this;
+
     $rootScope.isformad = true;
     $http.get(config.apiUrl+'?q=categories').success(function(data) {
-        $scope.categories = data;
+        $scope.ad={
+            categories: data,
+            categorySelected: {id: '3'}
+        };
     });
+
+    $scope.add = function(ad) {
+console.log(ad);
+    };
 }
