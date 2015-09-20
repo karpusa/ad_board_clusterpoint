273d0557a5c6209b900d6444e3a4cfdea9f729f5
Vitalij Karpusa <vitalij@karpusa.lv>
2015-09-20 19:57:10 +0300

    Add validation form, add ad post request

--- app/ad/add.controller.js
+++ app/ad/add.controller.js
@@ -11,11 +11,28 @@ function AddCtrl($rootScope, $scope, $http, config) {
     $http.get(config.apiUrl+'?q=categories').success(function(data) {
         $scope.ad={
             categories: data,
-            categorySelected: {id: '3'}
+            //categorySelected: {id: '3'}
         };
     });
 
-    $scope.add = function(ad) {
-console.log(ad);
+    $scope.add = function() {
+        $http({
+            method  : 'POST',
+            url     : config.apiUrl+'?q=ad/add',
+            data    :   {
+                            title:      $scope.ad.title,
+                            message:    $scope.ad.message,
+                            price:      $scope.ad.price,
+                            category:   $scope.ad.categorySelected.id
+                        },
+        })
+        .success(function(data) {
+
+        if (data.success) {
+            alert('success');
+            //@TODO redirect to ad post
+        }
+
+        });
     };
 }
